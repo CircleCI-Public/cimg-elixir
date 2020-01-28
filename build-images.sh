935ae40cb8d1b4a102f43094173c006aa0c9eaa9
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-01-28 13:04:37 -0500

    Add v1.10.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 1.9/Dockerfile -t cimg/elixir:1.9.4  -t cimg/elixir:1.9 .
+docker build --file 1.10/Dockerfile -t cimg/elixir:1.10.0  -t cimg/elixir:1.10 .
