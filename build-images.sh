17717c32897a3be2890b6277b778fdffb6434312
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-03-03 11:52:00 -0500

    Add v1.10.2. [release] (#21)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 1.10/Dockerfile -t cimg/elixir:1.10.1  -t cimg/elixir:1.10 .
+docker build --file 1.10/Dockerfile -t cimg/elixir:1.10.2  -t cimg/elixir:1.10 .
