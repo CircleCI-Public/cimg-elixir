010b46d6e82d58d06327b4f71e071c9a669958cd
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-07-13 15:06:03 -0400

    Respin v1.9.4 and v1.10.4 in prep for GA.

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+docker build --file 1.9/Dockerfile -t cimg/elixir:1.9.4  -t cimg/elixir:1.9 .
+docker build --file 1.9/node/Dockerfile -t cimg/elixir:1.9.4-node  -t cimg/elixir:1.9-node .
 docker build --file 1.10/Dockerfile -t cimg/elixir:1.10.4  -t cimg/elixir:1.10 .
+docker build --file 1.10/node/Dockerfile -t cimg/elixir:1.10.4-node  -t cimg/elixir:1.10-node .
