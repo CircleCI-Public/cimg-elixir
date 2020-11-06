627ab76d99fd0d7b42966e284d8691be20e358af
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-11-05 19:53:44 -0500

    Publish v1.11.2. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 1.11/Dockerfile -t cimg/elixir:1.11.1  -t cimg/elixir:1.11 .
-docker build --file 1.11/node/Dockerfile -t cimg/elixir:1.11.1-node  -t cimg/elixir:1.11-node .
-docker build --file 1.11/browsers/Dockerfile -t cimg/elixir:1.11.1-browsers  -t cimg/elixir:1.11-browsers .
+docker build --file 1.11/Dockerfile -t cimg/elixir:1.11.2  -t cimg/elixir:1.11 .
+docker build --file 1.11/node/Dockerfile -t cimg/elixir:1.11.2-node  -t cimg/elixir:1.11-node .
+docker build --file 1.11/browsers/Dockerfile -t cimg/elixir:1.11.2-browsers  -t cimg/elixir:1.11-browsers .
