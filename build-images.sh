e8eb8b42740eec9d3c3ed8b3574fad27cf008def
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-11-13 17:34:30 -0500

    Respin browsers tags for xvfb, part 2.

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 1.11/browsers/Dockerfile -t cimg/elixir:1.11.0-browsers  -t cimg/elixir:1.11-browsers .
+docker build --file 1.11/browsers/Dockerfile -t cimg/elixir:1.11.1-browsers  -t cimg/elixir:1.11-browsers .
