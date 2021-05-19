97dc002404b7aa969f871bddadd1582e6410b9c3
VitorTrin <emailreservadovitor@gmail.com>
2021-05-19 11:55:10 -0300

    Add 1.12 images

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 1.11/Dockerfile -t cimg/elixir:1.11.4  -t cimg/elixir:1.11 .
-docker build --file 1.11/node/Dockerfile -t cimg/elixir:1.11.4-node  -t cimg/elixir:1.11-node .
-docker build --file 1.11/browsers/Dockerfile -t cimg/elixir:1.11.4-browsers  -t cimg/elixir:1.11-browsers .
+docker build --file 1.12/Dockerfile -t cimg/elixir:1.12.0  -t cimg/elixir:1.12 .
+docker build --file 1.12/node/Dockerfile -t cimg/elixir:1.12.0-node  -t cimg/elixir:1.12-node .
+docker build --file 1.12/browsers/Dockerfile -t cimg/elixir:1.12.0-browsers  -t cimg/elixir:1.12-browsers .
