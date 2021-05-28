9cc36e1a159541d9f35d324eeb28aad490adab89
Adam Hartley <git@ahartley.com>
2021-05-28 17:59:31 +0100

    Publish v1.12.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 1.12/Dockerfile -t cimg/elixir:1.12.0  -t cimg/elixir:1.12 .
-docker build --file 1.12/node/Dockerfile -t cimg/elixir:1.12.0-node  -t cimg/elixir:1.12-node .
-docker build --file 1.12/browsers/Dockerfile -t cimg/elixir:1.12.0-browsers  -t cimg/elixir:1.12-browsers .
+docker build --file 1.12/Dockerfile -t cimg/elixir:1.12.1  -t cimg/elixir:1.12 .
+docker build --file 1.12/node/Dockerfile -t cimg/elixir:1.12.1-node  -t cimg/elixir:1.12-node .
+docker build --file 1.12/browsers/Dockerfile -t cimg/elixir:1.12.1-browsers  -t cimg/elixir:1.12-browsers .
