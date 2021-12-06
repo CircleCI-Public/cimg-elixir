478f0e1d40e9df0a4673e8421595eccd03dc3afb
Adam Hartley <BytesGuy@users.noreply.github.com>
2021-12-06 14:02:28 +0000

    Publish v1.13.0. [release] (#63)

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --file 1.12/Dockerfile -t cimg/elixir:1.12.3  -t cimg/elixir:1.12 .
-docker build --file 1.12/node/Dockerfile -t cimg/elixir:1.12.3-node  -t cimg/elixir:1.12-node .
-docker build --file 1.12/browsers/Dockerfile -t cimg/elixir:1.12.3-browsers  -t cimg/elixir:1.12-browsers .
+docker build --file 1.13/Dockerfile -t cimg/elixir:1.13.0  -t cimg/elixir:1.13 .
+docker build --file 1.13/node/Dockerfile -t cimg/elixir:1.13.0-node  -t cimg/elixir:1.13-node .
+docker build --file 1.13/browsers/Dockerfile -t cimg/elixir:1.13.0-browsers  -t cimg/elixir:1.13-browsers .
