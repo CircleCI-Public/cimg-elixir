77a7ad567b1f7155084addbfdaee615e33648308
Jeff Chen <jeff.chen@circleci.com>
2022-02-09 18:54:01 -0800

    q1 tooling and image change to main

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
 
 docker build --file 1.13/Dockerfile -t cimg/elixir:1.13.2  -t cimg/elixir:1.13 .
 docker build --file 1.13/node/Dockerfile -t cimg/elixir:1.13.2-node  -t cimg/elixir:1.13-node .
