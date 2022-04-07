66e000592e731b1e2f266f0c159d19a5e422017d
Jalexchen <jeff.chen@circleci.com>
2022-04-07 14:05:50 -0700

    Publish v1.13.4. [release] (#72)

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/elixir:1.13.3
+docker push cimg/elixir:1.13.4
 docker push cimg/elixir:1.13
-docker push cimg/elixir:1.13.3-node
+docker push cimg/elixir:1.13.4-node
 docker push cimg/elixir:1.13-node
-docker push cimg/elixir:1.13.3-browsers
+docker push cimg/elixir:1.13.4-browsers
 docker push cimg/elixir:1.13-browsers
