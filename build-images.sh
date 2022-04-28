0e5ff8f4c6047274b29e1996a9be06154ba76f5e
Jalexchen <jeff.chen@circleci.com>
2022-04-28 09:39:59 -0700

    Respin Elixir v1.13.4 with multiple Erlang version support [release] (#76)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.13/Dockerfile -t cimg/elixir:1.13.4  -t cimg/elixir:1.13 .
-docker build --file 1.13/node/Dockerfile -t cimg/elixir:1.13.4-node  -t cimg/elixir:1.13-node .
-docker build --file 1.13/browsers/Dockerfile -t cimg/elixir:1.13.4-browsers  -t cimg/elixir:1.13-browsers .
+docker build --file 1.13/22.3/Dockerfile -t cimg/elixir:1.13.4-erlang-22.3 -t cimg/elixir:1.13-erlang-22.3 .
+docker build --file 1.13/22.3/node/Dockerfile -t cimg/elixir:1.13.4-erlang-22.3-node -t cimg/elixir:1.13-erlang-22.3-node .
+docker build --file 1.13/22.3/browsers/Dockerfile -t cimg/elixir:1.13.4-erlang-22.3-browsers -t cimg/elixir:1.13-erlang-22.3-browsers .
+docker build --file 1.13/23.3/Dockerfile -t cimg/elixir:1.13.4-erlang-23.3 -t cimg/elixir:1.13-erlang-23.3 .
+docker build --file 1.13/23.3/node/Dockerfile -t cimg/elixir:1.13.4-erlang-23.3-node -t cimg/elixir:1.13-erlang-23.3-node .
+docker build --file 1.13/23.3/browsers/Dockerfile -t cimg/elixir:1.13.4-erlang-23.3-browsers -t cimg/elixir:1.13-erlang-23.3-browsers .
+docker build --file 1.13/24.3/Dockerfile -t cimg/elixir:1.13.4-erlang-24.3 -t cimg/elixir:1.13-erlang-24.3 .
+docker build --file 1.13/24.3/node/Dockerfile -t cimg/elixir:1.13.4-erlang-24.3-node -t cimg/elixir:1.13-erlang-24.3-node .
+docker build --file 1.13/24.3/browsers/Dockerfile -t cimg/elixir:1.13.4-erlang-24.3-browsers -t cimg/elixir:1.13-erlang-24.3-browsers .
