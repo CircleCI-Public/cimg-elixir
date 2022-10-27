bc8e7e3cb150a60bc11bf7635d4b9288b4512eb1
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-10-27 03:43:25 -0400

    Respin v1.14.1, 2. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.14/22.3.4/Dockerfile -t cimg/elixir:1.14.1-erlang-22.3.4 -t cimg/elixir:1.14-erlang-22.3.4 .
-docker build --file 1.14/22.3.4/node/Dockerfile -t cimg/elixir:1.14.1-erlang-22.3.4-node -t cimg/elixir:1.14-erlang-22.3.4-node .
-docker build --file 1.14/22.3.4/browsers/Dockerfile -t cimg/elixir:1.14.1-erlang-22.3.4-browsers -t cimg/elixir:1.14-erlang-22.3.4-browsers .
 docker build --file 1.14/23.3.1/Dockerfile -t cimg/elixir:1.14.1-erlang-23.3.1 -t cimg/elixir:1.14-erlang-23.3.1 .
 docker build --file 1.14/23.3.1/node/Dockerfile -t cimg/elixir:1.14.1-erlang-23.3.1-node -t cimg/elixir:1.14-erlang-23.3.1-node .
 docker build --file 1.14/23.3.1/browsers/Dockerfile -t cimg/elixir:1.14.1-erlang-23.3.1-browsers -t cimg/elixir:1.14-erlang-23.3.1-browsers .
