5296d871ea2ca5ef31e33c133ecb7ff64a859104
Jeff Chen <jeff.chen@circleci.com>
2023-05-23 09:45:42 -0700

    Publish v1.14.5. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.14/23.3.1/Dockerfile -t cimg/elixir:1.14.4-erlang-23.3.1 -t cimg/elixir:1.14-erlang-23.3.1 --platform linux/amd64 .
-docker build --file 1.14/23.3.1/node/Dockerfile -t cimg/elixir:1.14.4-erlang-23.3.1-node -t cimg/elixir:1.14-erlang-23.3.1-node --platform linux/amd64 .
-docker build --file 1.14/23.3.1/browsers/Dockerfile -t cimg/elixir:1.14.4-erlang-23.3.1-browsers -t cimg/elixir:1.14-erlang-23.3.1-browsers --platform linux/amd64 .
-docker build --file 1.14/24.3.3/Dockerfile -t cimg/elixir:1.14.4-erlang-24.3.3 -t cimg/elixir:1.14-erlang-24.3.3 --platform linux/amd64 .
-docker build --file 1.14/24.3.3/node/Dockerfile -t cimg/elixir:1.14.4-erlang-24.3.3-node -t cimg/elixir:1.14-erlang-24.3.3-node --platform linux/amd64 .
-docker build --file 1.14/24.3.3/browsers/Dockerfile -t cimg/elixir:1.14.4-erlang-24.3.3-browsers -t cimg/elixir:1.14-erlang-24.3.3-browsers --platform linux/amd64 .
-docker build --file 1.14/25.3/Dockerfile -t cimg/elixir:1.14.4-erlang-25.3 -t cimg/elixir:1.14-erlang-25.3 --platform linux/amd64 .
-docker build --file 1.14/25.3/node/Dockerfile -t cimg/elixir:1.14.4-erlang-25.3-node -t cimg/elixir:1.14-erlang-25.3-node --platform linux/amd64 .
-docker build --file 1.14/25.3/browsers/Dockerfile -t cimg/elixir:1.14.4-erlang-25.3-browsers -t cimg/elixir:1.14-erlang-25.3-browsers --platform linux/amd64 .
+docker build --file 1.14/23.3.1/Dockerfile -t cimg/elixir:1.14.5-erlang-23.3.1 -t cimg/elixir:1.14-erlang-23.3.1 --platform linux/amd64 .
+docker build --file 1.14/23.3.1/node/Dockerfile -t cimg/elixir:1.14.5-erlang-23.3.1-node -t cimg/elixir:1.14-erlang-23.3.1-node --platform linux/amd64 .
+docker build --file 1.14/23.3.1/browsers/Dockerfile -t cimg/elixir:1.14.5-erlang-23.3.1-browsers -t cimg/elixir:1.14-erlang-23.3.1-browsers --platform linux/amd64 .
+docker build --file 1.14/24.3.3/Dockerfile -t cimg/elixir:1.14.5-erlang-24.3.3 -t cimg/elixir:1.14-erlang-24.3.3 --platform linux/amd64 .
+docker build --file 1.14/24.3.3/node/Dockerfile -t cimg/elixir:1.14.5-erlang-24.3.3-node -t cimg/elixir:1.14-erlang-24.3.3-node --platform linux/amd64 .
+docker build --file 1.14/24.3.3/browsers/Dockerfile -t cimg/elixir:1.14.5-erlang-24.3.3-browsers -t cimg/elixir:1.14-erlang-24.3.3-browsers --platform linux/amd64 .
+docker build --file 1.14/25.3/Dockerfile -t cimg/elixir:1.14.5-erlang-25.3 -t cimg/elixir:1.14-erlang-25.3 --platform linux/amd64 .
+docker build --file 1.14/25.3/node/Dockerfile -t cimg/elixir:1.14.5-erlang-25.3-node -t cimg/elixir:1.14-erlang-25.3-node --platform linux/amd64 .
+docker build --file 1.14/25.3/browsers/Dockerfile -t cimg/elixir:1.14.5-erlang-25.3-browsers -t cimg/elixir:1.14-erlang-25.3-browsers --platform linux/amd64 .
