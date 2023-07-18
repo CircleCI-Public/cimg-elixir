dfca1d0796f5da7c80dd824f38cd7ebe55f0d215
Jalexchen <jeff.chen@circleci.com>
2023-07-18 12:16:46 -0700

    Publish v1.15.4. [release] (#122)

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 1.15/24.3.3/Dockerfile -t cimg/elixir:1.15-erlang-24.3.3 -t cimg/elixir:1.15-erlang-24.3.3 --platform linux/amd64 .
-docker build --file 1.15/24.3.3/node/Dockerfile -t cimg/elixir:1.15-erlang-24.3.3-node -t cimg/elixir:1.15-erlang-24.3.3-node --platform linux/amd64 .
-docker build --file 1.15/24.3.3/browsers/Dockerfile -t cimg/elixir:1.15-erlang-24.3.3-browsers -t cimg/elixir:1.15-erlang-24.3.3-browsers --platform linux/amd64 .
-docker build --file 1.15/25.3/Dockerfile -t cimg/elixir:1.15-erlang-25.3 -t cimg/elixir:1.15-erlang-25.3 --platform linux/amd64 .
-docker build --file 1.15/25.3/node/Dockerfile -t cimg/elixir:1.15-erlang-25.3-node -t cimg/elixir:1.15-erlang-25.3-node --platform linux/amd64 .
-docker build --file 1.15/25.3/browsers/Dockerfile -t cimg/elixir:1.15-erlang-25.3-browsers -t cimg/elixir:1.15-erlang-25.3-browsers --platform linux/amd64 .
+docker build --file 1.15/24.3.3/Dockerfile -t cimg/elixir:1.15.4-erlang-24.3.3 -t cimg/elixir:1.15-erlang-24.3.3 --platform linux/amd64 .
+docker build --file 1.15/24.3.3/node/Dockerfile -t cimg/elixir:1.15.4-erlang-24.3.3-node -t cimg/elixir:1.15-erlang-24.3.3-node --platform linux/amd64 .
+docker build --file 1.15/24.3.3/browsers/Dockerfile -t cimg/elixir:1.15.4-erlang-24.3.3-browsers -t cimg/elixir:1.15-erlang-24.3.3-browsers --platform linux/amd64 .
+docker build --file 1.15/25.3/Dockerfile -t cimg/elixir:1.15.4-erlang-25.3 -t cimg/elixir:1.15-erlang-25.3 --platform linux/amd64 .
+docker build --file 1.15/25.3/node/Dockerfile -t cimg/elixir:1.15.4-erlang-25.3-node -t cimg/elixir:1.15-erlang-25.3-node --platform linux/amd64 .
+docker build --file 1.15/25.3/browsers/Dockerfile -t cimg/elixir:1.15.4-erlang-25.3-browsers -t cimg/elixir:1.15-erlang-25.3-browsers --platform linux/amd64 .
