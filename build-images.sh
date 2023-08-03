e250a163bd411cf07e1dbda8f7446fbd423645d5
Adam Hartley <git@ahartley.com>
2023-08-03 11:58:24 +0100

    Run test build

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+set -eo pipefail
 
-docker build --file 1.15/24.3.3/Dockerfile -t cimg/elixir:1.15.4-erlang-24.3.3 -t cimg/elixir:1.15-erlang-24.3.3 --platform linux/amd64 .
-docker build --file 1.15/24.3.3/node/Dockerfile -t cimg/elixir:1.15.4-erlang-24.3.3-node -t cimg/elixir:1.15-erlang-24.3.3-node --platform linux/amd64 .
-docker build --file 1.15/24.3.3/browsers/Dockerfile -t cimg/elixir:1.15.4-erlang-24.3.3-browsers -t cimg/elixir:1.15-erlang-24.3.3-browsers --platform linux/amd64 .
-docker build --file 1.15/25.3/Dockerfile -t cimg/elixir:1.15.4-erlang-25.3 -t cimg/elixir:1.15-erlang-25.3 --platform linux/amd64 .
-docker build --file 1.15/25.3/node/Dockerfile -t cimg/elixir:1.15.4-erlang-25.3-node -t cimg/elixir:1.15-erlang-25.3-node --platform linux/amd64 .
-docker build --file 1.15/25.3/browsers/Dockerfile -t cimg/elixir:1.15.4-erlang-25.3-browsers -t cimg/elixir:1.15-erlang-25.3-browsers --platform linux/amd64 .
+docker context create cimg
+docker buildx create --use cimg
+docker buildx build --platform=linux/amd64,linux/arm64 --file 1.15/24.3.3/Dockerfile -t cimg/elixir:1.15.4-erlang-24.3.3 -t cimg/elixir:1.15-erlang-24.3.3 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 1.15/24.3.3/node/Dockerfile -t cimg/elixir:1.15.4-erlang-24.3.3-node -t cimg/elixir:1.15-erlang-24.3.3-node --push .
+docker buildx build --platform=linux/amd64 --file 1.15/24.3.3/browsers/Dockerfile -t cimg/elixir:1.15.4-erlang-24.3.3-browsers -t cimg/elixir:1.15-erlang-24.3.3-browsers --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 1.15/25.3/Dockerfile -t cimg/elixir:1.15.4-erlang-25.3 -t cimg/elixir:1.15-erlang-25.3 --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 1.15/25.3/node/Dockerfile -t cimg/elixir:1.15.4-erlang-25.3-node -t cimg/elixir:1.15-erlang-25.3-node --push .
+docker buildx build --platform=linux/amd64 --file 1.15/25.3/browsers/Dockerfile -t cimg/elixir:1.15.4-erlang-25.3-browsers -t cimg/elixir:1.15-erlang-25.3-browsers --push .
--- push-images.sh
+++ push-images.sh
@@ -1,26 +1,3 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/elixir:1.15-erlang-24.3.3
-docker push cimg/elixir:1.15.4-erlang-24.3.3
-docker push cimg/elixir:1.15-erlang-24.3.3-node
-docker push cimg/elixir:1.15.4-erlang-24.3.3-node
-docker push cimg/elixir:1.15-erlang-24.3.3-browsers
-docker push cimg/elixir:1.15.4-erlang-24.3.3-browsers
-docker push cimg/elixir:1.15-erlang-25.3
-docker push cimg/elixir:1.15.4-erlang-25.3
-docker tag cimg/elixir:1.15.4-erlang-25.3 cimg/elixir:1.15.4
-docker tag cimg/elixir:1.15-erlang-25.3 cimg/elixir:1.15
-docker push cimg/elixir:1.15
-docker push cimg/elixir:1.15.4
-docker push cimg/elixir:1.15-erlang-25.3-node
-docker push cimg/elixir:1.15.4-erlang-25.3-node
-docker tag cimg/elixir:1.15.4-erlang-25.3-node cimg/elixir:1.15.4-node
-docker tag cimg/elixir:1.15-erlang-25.3-node cimg/elixir:1.15-node
-docker push cimg/elixir:1.15-node
-docker push cimg/elixir:1.15.4-node
-docker push cimg/elixir:1.15-erlang-25.3-browsers
-docker push cimg/elixir:1.15.4-erlang-25.3-browsers
-docker tag cimg/elixir:1.15.4-erlang-25.3-browsers cimg/elixir:1.15.4-browsers
-docker tag cimg/elixir:1.15-erlang-25.3-browsers cimg/elixir:1.15-browsers
-docker push cimg/elixir:1.15-browsers
-docker push cimg/elixir:1.15.4-browsers
+set -eo pipefail
