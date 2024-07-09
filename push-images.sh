a52ceb1b5f5ea177c9ebbea6590792dffb8eeb44
Bryan Johnson <bryan@circleci.com>
2024-07-09 09:08:42 -0400

    Remove invalid combinations manually (for now)

--- push-images.sh
+++ push-images.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 set -eo pipefail
-docker push cimg/elixir:1.17-erlang-24.3.4
-docker push cimg/elixir:1.17.2-erlang-24.3.4
-docker push cimg/elixir:1.17-erlang-24.3.4-node
-docker push cimg/elixir:1.17.2-erlang-24.3.4-node
-docker push cimg/elixir:1.17-erlang-24.3.4-browsers
-docker push cimg/elixir:1.17.2-erlang-24.3.4-browsers
 docker push cimg/elixir:1.17-erlang-25.3.2
 docker push cimg/elixir:1.17.2-erlang-25.3.2
 docker push cimg/elixir:1.17-erlang-25.3.2-node
@@ -31,9 +25,6 @@ docker tag cimg/elixir:1.17.2-erlang-26.2.1-browsers cimg/elixir:1.17.2-browsers
 docker tag cimg/elixir:1.17-erlang-26.2.1-browsers cimg/elixir:1.17-browsers
 docker push cimg/elixir:1.17-browsers
 docker push cimg/elixir:1.17.2-browsers
-docker push cimg/elixir:1.17.1-erlang-24.3.4
-docker push cimg/elixir:1.17.1-erlang-24.3.4-node
-docker push cimg/elixir:1.17.1-erlang-24.3.4-browsers
 docker push cimg/elixir:1.17.1-erlang-25.3.2
 docker push cimg/elixir:1.17.1-erlang-25.3.2-node
 docker push cimg/elixir:1.17.1-erlang-25.3.2-browsers
@@ -46,9 +37,6 @@ docker push cimg/elixir:1.17.1-node
 docker push cimg/elixir:1.17.1-erlang-26.2.1-browsers
 docker tag cimg/elixir:1.17.1-erlang-26.2.1-browsers cimg/elixir:1.17.1-browsers
 docker push cimg/elixir:1.17.1-browsers
-docker push cimg/elixir:1.17.0-erlang-24.3.4
-docker push cimg/elixir:1.17.0-erlang-24.3.4-node
-docker push cimg/elixir:1.17.0-erlang-24.3.4-browsers
 docker push cimg/elixir:1.17.0-erlang-25.3.2
 docker push cimg/elixir:1.17.0-erlang-25.3.2-node
 docker push cimg/elixir:1.17.0-erlang-25.3.2-browsers
