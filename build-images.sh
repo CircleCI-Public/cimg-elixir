39d40ffb5eac064bd6955f588a665986a23b0e53
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-01-24 14:13:30 -0500

    Add version 1.16.3

--- build-images.sh
+++ build-images.sh
@@ -32,15 +32,3 @@ docker build --file 1.16/25.3.2/browsers/Dockerfile -t cimg/elixir:1.16.3-erlang
 docker build --file 1.16/26.2.1/Dockerfile -t cimg/elixir:1.16.3-erlang-26.2.1 -t cimg/elixir:1.16-erlang-26.2.1 --platform linux/amd64 --push .
 docker build --file 1.16/26.2.1/node/Dockerfile -t cimg/elixir:1.16.3-erlang-26.2.1-node -t cimg/elixir:1.16-erlang-26.2.1-node --platform linux/amd64 --push .
 docker build --file 1.16/26.2.1/browsers/Dockerfile -t cimg/elixir:1.16.3-erlang-26.2.1-browsers -t cimg/elixir:1.16-erlang-26.2.1-browsers --platform linux/amd64 --push .
-docker build --file 1.15/25.3.2/Dockerfile -t cimg/elixir:1.15.8-erlang-25.3.2 -t cimg/elixir:1.15-erlang-25.3.2 --platform linux/amd64 --push .
-docker build --file 1.15/25.3.2/node/Dockerfile -t cimg/elixir:1.15.8-erlang-25.3.2-node -t cimg/elixir:1.15-erlang-25.3.2-node --platform linux/amd64 --push .
-docker build --file 1.15/25.3.2/browsers/Dockerfile -t cimg/elixir:1.15.8-erlang-25.3.2-browsers -t cimg/elixir:1.15-erlang-25.3.2-browsers --platform linux/amd64 --push .
-docker build --file 1.15/26.2.1/Dockerfile -t cimg/elixir:1.15.8-erlang-26.2.1 -t cimg/elixir:1.15-erlang-26.2.1 --platform linux/amd64 --push .
-docker build --file 1.15/26.2.1/node/Dockerfile -t cimg/elixir:1.15.8-erlang-26.2.1-node -t cimg/elixir:1.15-erlang-26.2.1-node --platform linux/amd64 --push .
-docker build --file 1.15/26.2.1/browsers/Dockerfile -t cimg/elixir:1.15.8-erlang-26.2.1-browsers -t cimg/elixir:1.15-erlang-26.2.1-browsers --platform linux/amd64 --push .
-docker build --file 1.16/25.3.2/Dockerfile -t cimg/elixir:1.16.2-erlang-25.3.2 -t cimg/elixir:1.16-erlang-25.3.2 --platform linux/amd64 --push .
-docker build --file 1.16/25.3.2/node/Dockerfile -t cimg/elixir:1.16.2-erlang-25.3.2-node -t cimg/elixir:1.16-erlang-25.3.2-node --platform linux/amd64 --push .
-docker build --file 1.16/25.3.2/browsers/Dockerfile -t cimg/elixir:1.16.2-erlang-25.3.2-browsers -t cimg/elixir:1.16-erlang-25.3.2-browsers --platform linux/amd64 --push .
-docker build --file 1.16/26.2.1/Dockerfile -t cimg/elixir:1.16.2-erlang-26.2.1 -t cimg/elixir:1.16-erlang-26.2.1 --platform linux/amd64 --push .
-docker build --file 1.16/26.2.1/node/Dockerfile -t cimg/elixir:1.16.2-erlang-26.2.1-node -t cimg/elixir:1.16-erlang-26.2.1-node --platform linux/amd64 --push .
-docker build --file 1.16/26.2.1/browsers/Dockerfile -t cimg/elixir:1.16.2-erlang-26.2.1-browsers -t cimg/elixir:1.16-erlang-26.2.1-browsers --platform linux/amd64 --push .
