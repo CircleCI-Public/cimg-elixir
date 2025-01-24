59b158017f9687ca21f8d8dd5cecef9bdc10919c
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-01-24 15:44:44 -0500

    Pub: 1.18.2,1.18.1,1.18.0,1.17.3, and more. [release]

--- push-images.sh
+++ push-images.sh
@@ -25,6 +25,12 @@ docker tag cimg/elixir:1.18.2-erlang-26.2.1-browsers cimg/elixir:1.18.2-browsers
 docker tag cimg/elixir:1.18-erlang-26.2.1-browsers cimg/elixir:1.18-browsers
 docker push cimg/elixir:1.18-browsers
 docker push cimg/elixir:1.18.2-browsers
+docker push cimg/elixir:1.18-erlang-27.2.1
+docker push cimg/elixir:1.18.2-erlang-27.2.1
+docker push cimg/elixir:1.18-erlang-27.2.1-node
+docker push cimg/elixir:1.18.2-erlang-27.2.1-node
+docker push cimg/elixir:1.18-erlang-27.2.1-browsers
+docker push cimg/elixir:1.18.2-erlang-27.2.1-browsers
 docker push cimg/elixir:1.18.1-erlang-25.3.2
 docker push cimg/elixir:1.18.1-erlang-25.3.2-node
 docker push cimg/elixir:1.18.1-erlang-25.3.2-browsers
@@ -37,6 +43,9 @@ docker push cimg/elixir:1.18.1-node
 docker push cimg/elixir:1.18.1-erlang-26.2.1-browsers
 docker tag cimg/elixir:1.18.1-erlang-26.2.1-browsers cimg/elixir:1.18.1-browsers
 docker push cimg/elixir:1.18.1-browsers
+docker push cimg/elixir:1.18.1-erlang-27.2.1
+docker push cimg/elixir:1.18.1-erlang-27.2.1-node
+docker push cimg/elixir:1.18.1-erlang-27.2.1-browsers
 docker push cimg/elixir:1.18.0-erlang-25.3.2
 docker push cimg/elixir:1.18.0-erlang-25.3.2-node
 docker push cimg/elixir:1.18.0-erlang-25.3.2-browsers
@@ -49,6 +58,9 @@ docker push cimg/elixir:1.18.0-node
 docker push cimg/elixir:1.18.0-erlang-26.2.1-browsers
 docker tag cimg/elixir:1.18.0-erlang-26.2.1-browsers cimg/elixir:1.18.0-browsers
 docker push cimg/elixir:1.18.0-browsers
+docker push cimg/elixir:1.18.0-erlang-27.2.1
+docker push cimg/elixir:1.18.0-erlang-27.2.1-node
+docker push cimg/elixir:1.18.0-erlang-27.2.1-browsers
 docker push cimg/elixir:1.17-erlang-25.3.2
 docker push cimg/elixir:1.17.3-erlang-25.3.2
 docker push cimg/elixir:1.17-erlang-25.3.2-node
@@ -73,6 +85,12 @@ docker tag cimg/elixir:1.17.3-erlang-26.2.1-browsers cimg/elixir:1.17.3-browsers
 docker tag cimg/elixir:1.17-erlang-26.2.1-browsers cimg/elixir:1.17-browsers
 docker push cimg/elixir:1.17-browsers
 docker push cimg/elixir:1.17.3-browsers
+docker push cimg/elixir:1.17-erlang-27.2.1
+docker push cimg/elixir:1.17.3-erlang-27.2.1
+docker push cimg/elixir:1.17-erlang-27.2.1-node
+docker push cimg/elixir:1.17.3-erlang-27.2.1-node
+docker push cimg/elixir:1.17-erlang-27.2.1-browsers
+docker push cimg/elixir:1.17.3-erlang-27.2.1-browsers
 docker push cimg/elixir:1.17.2-erlang-25.3.2
 docker push cimg/elixir:1.17.2-erlang-25.3.2-node
 docker push cimg/elixir:1.17.2-erlang-25.3.2-browsers
@@ -85,6 +103,9 @@ docker push cimg/elixir:1.17.2-node
 docker push cimg/elixir:1.17.2-erlang-26.2.1-browsers
 docker tag cimg/elixir:1.17.2-erlang-26.2.1-browsers cimg/elixir:1.17.2-browsers
 docker push cimg/elixir:1.17.2-browsers
+docker push cimg/elixir:1.17.2-erlang-27.2.1
+docker push cimg/elixir:1.17.2-erlang-27.2.1-node
+docker push cimg/elixir:1.17.2-erlang-27.2.1-browsers
 docker push cimg/elixir:1.17.1-erlang-25.3.2
 docker push cimg/elixir:1.17.1-erlang-25.3.2-node
 docker push cimg/elixir:1.17.1-erlang-25.3.2-browsers
@@ -97,4 +118,21 @@ docker push cimg/elixir:1.17.1-node
 docker push cimg/elixir:1.17.1-erlang-26.2.1-browsers
 docker tag cimg/elixir:1.17.1-erlang-26.2.1-browsers cimg/elixir:1.17.1-browsers
 docker push cimg/elixir:1.17.1-browsers
-
+docker push cimg/elixir:1.17.1-erlang-27.2.1
+docker push cimg/elixir:1.17.1-erlang-27.2.1-node
+docker push cimg/elixir:1.17.1-erlang-27.2.1-browsers
+docker push cimg/elixir:1.17.0-erlang-25.3.2
+docker push cimg/elixir:1.17.0-erlang-25.3.2-node
+docker push cimg/elixir:1.17.0-erlang-25.3.2-browsers
+docker push cimg/elixir:1.17.0-erlang-26.2.1
+docker tag cimg/elixir:1.17.0-erlang-26.2.1 cimg/elixir:1.17.0
+docker push cimg/elixir:1.17.0
+docker push cimg/elixir:1.17.0-erlang-26.2.1-node
+docker tag cimg/elixir:1.17.0-erlang-26.2.1-node cimg/elixir:1.17.0-node
+docker push cimg/elixir:1.17.0-node
+docker push cimg/elixir:1.17.0-erlang-26.2.1-browsers
+docker tag cimg/elixir:1.17.0-erlang-26.2.1-browsers cimg/elixir:1.17.0-browsers
+docker push cimg/elixir:1.17.0-browsers
+docker push cimg/elixir:1.17.0-erlang-27.2.1
+docker push cimg/elixir:1.17.0-erlang-27.2.1-node
+docker push cimg/elixir:1.17.0-erlang-27.2.1-browsers
---
+++ restore-changes.sh
@@ -0,0 +1,6 @@
+git restore 1.15
+git restore 1.16
+git restore 1.17
+git restore push-images.sh
+git restore GEN-CHECK
+git restore build-images.sh
