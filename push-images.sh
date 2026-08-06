8e530a6835cb529336c04fbf2db76d2b022c9907
Anthony Castro <anthony.castro@circleci.com>
2026-08-06 09:40:58 -0400

    Regenerate 1.20

--- push-images.sh
+++ push-images.sh
@@ -25,3 +25,33 @@ docker tag cimg/elixir:1.19.5-erlang-28.5.0-browsers cimg/elixir:1.19.5-browsers
 docker tag cimg/elixir:1.19-erlang-28.5.0-browsers cimg/elixir:1.19-browsers
 docker push cimg/elixir:1.19-browsers
 docker push cimg/elixir:1.19.5-browsers
+docker push cimg/elixir:1.20-erlang-27.3.4
+docker push cimg/elixir:1.20.0-erlang-27.3.4
+docker push cimg/elixir:1.20-erlang-27.3.4-node
+docker push cimg/elixir:1.20.0-erlang-27.3.4-node
+docker push cimg/elixir:1.20-erlang-27.3.4-browsers
+docker push cimg/elixir:1.20.0-erlang-27.3.4-browsers
+docker push cimg/elixir:1.20-erlang-28.5.0
+docker push cimg/elixir:1.20.0-erlang-28.5.0
+docker push cimg/elixir:1.20-erlang-28.5.0-node
+docker push cimg/elixir:1.20.0-erlang-28.5.0-node
+docker push cimg/elixir:1.20-erlang-28.5.0-browsers
+docker push cimg/elixir:1.20.0-erlang-28.5.0-browsers
+docker push cimg/elixir:1.20-erlang-29.0.5
+docker push cimg/elixir:1.20.0-erlang-29.0.5
+docker tag cimg/elixir:1.20.0-erlang-29.0.5 cimg/elixir:1.20.0
+docker tag cimg/elixir:1.20-erlang-29.0.5 cimg/elixir:1.20
+docker push cimg/elixir:1.20
+docker push cimg/elixir:1.20.0
+docker push cimg/elixir:1.20-erlang-29.0.5-node
+docker push cimg/elixir:1.20.0-erlang-29.0.5-node
+docker tag cimg/elixir:1.20.0-erlang-29.0.5-node cimg/elixir:1.20.0-node
+docker tag cimg/elixir:1.20-erlang-29.0.5-node cimg/elixir:1.20-node
+docker push cimg/elixir:1.20-node
+docker push cimg/elixir:1.20.0-node
+docker push cimg/elixir:1.20-erlang-29.0.5-browsers
+docker push cimg/elixir:1.20.0-erlang-29.0.5-browsers
+docker tag cimg/elixir:1.20.0-erlang-29.0.5-browsers cimg/elixir:1.20.0-browsers
+docker tag cimg/elixir:1.20-erlang-29.0.5-browsers cimg/elixir:1.20-browsers
+docker push cimg/elixir:1.20-browsers
+docker push cimg/elixir:1.20.0-browsers
